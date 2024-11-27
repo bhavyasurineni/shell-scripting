54c904d21ee77852deb9403ee7b96ee439032f7d
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-11-27 13:35:03 +0530

    .

--- print.sh
+++ print.sh
@@ -6,5 +6,5 @@ echo -e "\e[31mHI\e[32mTHIS\e[33mIS\e[34m--\e[35mBHAVYA\e[36m--.\e[0m]"
 
 echo -e "\e[1;31mHI\e[1;32mTHIS\e[1;33mIS\e[1;34m--\e[1;35mBHAVYA\e[4;36m--.\e[0m]"
 
-echo -e "line1/n line2"
-echo -e "word1/t/t/t word2"
+echo -e "line1\nline2"
+echo -e "word1\t\t\tword2"
