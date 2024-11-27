dfcb6652625ed0325a7ad1378e267ea89e13f1ab
bhavyasurineni <bhavyasurinnei@gmail.com>
2024-11-27 20:11:05 +0530

    variables date

--- variiable.sh
+++ variiable.sh
@@ -4,3 +4,9 @@ student_name="bhavya"
 
 echo student_name=$student_name
 echo student_name=${student_name}
+
+date=2024-11-27
+echo Good Morning,Today date is $date
+
+Date=$(date +%F)
+echo Good Morning,Today date is $Date
